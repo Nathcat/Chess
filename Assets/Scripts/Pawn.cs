242fbd2cad4d8d6acb74a051d4e283b38b414a75
Nathcat <supernath126@gmail.com>
2021-09-15 16:21:49 +0100

    Added comments on Pawn.cs

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pawn : ChessPiece
+
+/*
+ * Pawn.cs
+ *
+ * ChessPiece script for the Pawn piece.
+ * The Pawn is a bit annoying, because it moves and attacks in different
+ * directions depending on which side it's on.
+ *
+ * Author: Nathan "Nathcat" Baines
+ */
+
+
+public class Pawn : ChessPiece  // Derive from ChessPiece parent class
 {
-    private GameManager gameManager;
-    private Vector3[] legalMoves;
-    public Vector3[] attackSquares;
+    private GameManager gameManager;  // Holds the GameManager script
+    private Vector3[] legalMoves;  // Array to hold the legal moves for this piece
+    public Vector3[] attackSquares;  // Array to hold the spaces this piece can attack
 
     public int side;
     private int oppositeSide;
@@ -14,10 +26,12 @@ public class Pawn : ChessPiece
 
     public bool moving = false;
 
-    void Start() {
+    void Start() {  // Start is called before the first frame in which this object is present
+        // Get the GameManager script from the object that hosts it ("GameManager")
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
         if (side == 0) {
+            // If this is a white piece, moves should go up the board
             attackSquares = new Vector3[] {
                 new Vector3(1.0f, 0.0f, 1.0f),
                 new Vector3(-1.0f, 0.0f, 1.0f)
@@ -31,6 +45,7 @@ public class Pawn : ChessPiece
             oppositeSide = 1;
 
         } else {
+            // Else they should go down the board
             attackSquares = new Vector3[] {
                 new Vector3(1.0f, 0.0f, -1.0f),
                 new Vector3(-1.0f, 0.0f, -1.0f)
@@ -46,30 +61,40 @@ public class Pawn : ChessPiece
         }
     }
 
-    void OnMouseDown() {
+    void OnMouseDown() {  // Called when this object is clicked
 
+        // If its this piece's side's turn, and this piece is not moving...
         if (gameManager.turn == side && !moving) {
+            // Destroy all existing move tokens
             foreach (GameObject moveToken in GameObject.FindGameObjectsWithTag("LegalMoveToken")) {
                 Destroy(moveToken);
             }
 
             foreach (Vector3 legalMove in legalMoves) {
+                // Check for a piece in the current legal move being checked
                 Collider[] piecesInWay = Physics.OverlapSphere(transform.position + legalMove, 0.5f);
 
-                if (piecesInWay.Length == 0) {
+                if (piecesInWay.Length == 0) {  // If there are no pieces in the legal move space
+                    // Create a move token
                     GameObject moveToken = Instantiate(gameManager.legalMoveToken, transform.position + legalMove, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
-
+                    // Set the parent piece of the move token to this piece
                     moveToken.GetComponent<MoveToken>().parentPiece = this;
                 }
             }
 
             foreach (Vector3 attackSquare in attackSquares) {
+                // Check the attack squares for pieces
                 Collider[] attackedPieceCollider = Physics.OverlapSphere(transform.position + attackSquare, 0.5f);
 
+                // If there is a piece in the current attack square
                 if (attackedPieceCollider.Length == 1) {
+                    // If the piece in the attack square is on the opposite side
                     if (attackedPieceCollider[0].transform.gameObject.CompareTag(sideNames[oppositeSide])) {
+                        // Create an attack token
                         GameObject attackToken = Instantiate(gameManager.attackToken, transform.position + attackSquare, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+                        // Set the parent piece to this piece
                         attackToken.GetComponent<AttackToken>().parentPiece = this;
+                        // Set the attacked piece to the piece in the square
                         attackToken.GetComponent<AttackToken>().attackedPiece = attackedPieceCollider[0].transform.gameObject;
                     }
                 }
@@ -78,16 +103,20 @@ public class Pawn : ChessPiece
 
     }
 
-    override public void move(GameObject legalMoveToken) {
+    override public void move(GameObject legalMoveToken) {  // Override move method
 
+        // Start a coroutine to move this piece over time to the new position
         StartCoroutine(moveOverTime(legalMoveToken.transform.position));
 
+        // Destroy all existing move tokens
         foreach (GameObject moveToken in GameObject.FindGameObjectsWithTag("LegalMoveToken")) {
             Destroy(moveToken);
         }
 
+        // Pawns can move two spaces on their first move, so set the legal moves array to only have one move in them
+        // because pawns cannot move two spaces after their first move.
         if (side == 0) { legalMoves = new Vector3[] { new Vector3(0.0f, 0.0f, 1.0f) }; } else { legalMoves = new Vector3[] { new Vector3(0.0f, 0.0f, -1.0f) }; }
 
-        gameManager.togglePlayer();
+        gameManager.togglePlayer();  // Move to the next player's turn
     }
 }
